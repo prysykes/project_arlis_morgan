299274975f9255ac7b4ea0d1be01545228540435
Chukwuemeka Duru <70113615+prysykes@users.noreply.github.com>
2024-05-31 22:10:02 -0400

    Update settings.py
    Signed-off-by: Chukwuemeka Duru <70113615+prysykes@users.noreply.github.com>

--- arlis/settings.py
+++ arlis/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-79c48yykt7rv4zi=qz!5h7*i^vx922-!%vfcuy@t$$lz71mzj('
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
