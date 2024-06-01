a9c2a60415911c19d8ce9dac1b2cce4d091d0a64
Chukwuemeka Duru <70113615+prysykes@users.noreply.github.com>
2024-05-31 22:12:24 -0400

    Update settings.py
    Signed-off-by: Chukwuemeka Duru <70113615+prysykes@users.noreply.github.com>

--- arlis/settings.py
+++ arlis/settings.py
@@ -151,6 +151,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'crediblereviewsng@gmail.com'
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-EMAIL_HOST_PASSWORD = 'rctwlfsuzojuytka'
